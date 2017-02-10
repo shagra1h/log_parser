3e824c4503ac63eb2797d169690bc0713eb2558b
shagra1h <achkael.serg@yandex.ru>
2017-02-10 19:05:29 +0300

    Подсчет и вывод в файл

--- log_parser.py
+++ log_parser.py
@@ -1,6 +1,7 @@
 import sys
 import os.path
 import os
+import datetime
 
 
 def check_path(filepath):
@@ -15,9 +16,9 @@ if __name__ == '__main__':
         sys.exit(0)
     if check_path(path_to_file):
         input_file = open(path_to_file, 'r')
-        out_uniq_steamids = open('join_log_uniq_steamids.txt', 'w')
-        out_uniq_ips = open('join_log_uniq_ips.txt', 'w')
-        out_uniq_ips_and_steamids = open('join_log_uniq_ips_and_steamids.txt', 'w')
+        uniq_steamids = 0
+        uniq_ips = 0
+        uniq_ips_and_steamids = 0
         steams = []
         ips = []
         input_file.readline()
@@ -26,19 +27,21 @@ if __name__ == '__main__':
             steamid = steamid[9:]
             ip = ip[4:]
             if (steamid not in steams) and (ip not in ips):
-                out_uniq_ips_and_steamids.write(line)
+                uniq_ips_and_steamids += 1
             if steamid not in steams:
                 steams.append(steamid)
-                out_uniq_steamids.write(line)
+                uniq_steamids += 1
             if ip not in ips:
                 ips.append(ip)
-                out_uniq_ips.write(line)
-        out_uniq_steamids.close()
-        out_uniq_ips.close()
-        out_uniq_ips_and_steamids.close()
+                uniq_ips += 1
         input_file.close()
-        path = os.getcwd()
-        print("Успешно! Отсортированные файлы: \njoin_log_uniq_steamids.txt\njoin_log_uniq_ips.txt\njoin_log_uniq_ips_and_steamids.txt\nнаходятся в папке", path)
+        output_filename = 'results_' + datetime.datetime.now().strftime("%d_%m_%Y") + '.txt'
+        output_file = open(output_filename, 'w')
+        output_file.write(" Уникальных steamid : " + str(uniq_steamids) + '\n')
+        output_file.write(" Уникальных ip : " + str(uniq_ips) + '\n')
+        output_file.write(" Уникальных комбинаций steamid + ip : " + str(uniq_ips_and_steamids))
+        output_file.close()
+        print("Успешно! результаты в файле", output_filename)
     else:
         print("Ошибка! Введите корректный путь и попробуйте еще раз!")
         sys.exit(0)
