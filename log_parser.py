6a7aa2edaf57329cb5efdd938f008c519ff88233
shagra1h <achkael.serg@yandex.ru>
2017-02-12 14:33:59 +0300

    файл зашит вкод

--- log_parser.py
+++ log_parser.py
@@ -1,47 +1,38 @@
 import sys
 import os.path
 import os
-import datetime
+from datetime import datetime
 
 
-def check_path(filepath):
-    if os.path.exists(filepath):
-        return 1
+def write_to_file(uniqIps, uniqSteamids, uniqIpsAndSteamids):
+    output_filename = 'results_' + datetime.now().strftime("%d_%m_%Y") + '.txt'
+    output_file = open(output_filename, 'w')
+    output_file.write(" Уникальных steamid : " + str(uniqSteamids) + '\n')
+    output_file.write(" Уникальных ip : " + str(uniqIps) + '\n')
+    output_file.write(" Уникальных комбинаций steamid + ip : " + str(uniqIpsAndSteamids))
+    output_file.close()
+    print("Успешно! результаты в файле", output_filename)
+
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        path_to_file = sys.argv[1]
-    else:
-        print("Необходимо указать путь к файлу!")
-        sys.exit(0)
-    if check_path(path_to_file):
-        input_file = open(path_to_file, 'r')
-        uniq_steamids = 0
-        uniq_ips = 0
-        uniq_ips_and_steamids = 0
-        steams = []
-        ips = []
-        input_file.readline()
-        for line in input_file:
-            line1, ip, steamid = line.split(" | ", maxsplit=2)
-            steamid = steamid[9:]
-            ip = ip[4:]
-            if (steamid not in steams) and (ip not in ips):
-                uniq_ips_and_steamids += 1
-            if steamid not in steams:
-                steams.append(steamid)
-                uniq_steamids += 1
-            if ip not in ips:
-                ips.append(ip)
-                uniq_ips += 1
-        input_file.close()
-        output_filename = 'results_' + datetime.datetime.now().strftime("%d_%m_%Y") + '.txt'
-        output_file = open(output_filename, 'w')
-        output_file.write(" Уникальных steamid : " + str(uniq_steamids) + '\n')
-        output_file.write(" Уникальных ip : " + str(uniq_ips) + '\n')
-        output_file.write(" Уникальных комбинаций steamid + ip : " + str(uniq_ips_and_steamids))
-        output_file.close()
-        print("Успешно! результаты в файле", output_filename)
-    else:
-        print("Ошибка! Введите корректный путь и попробуйте еще раз!")
-        sys.exit(0)
+    input_file = open('join_log.log', 'r')
+    uniq_steamids = 0
+    uniq_ips = 0
+    uniq_ips_and_steamids = 0
+    steams = []
+    ips = []
+    input_file.readline()
+    for line in input_file:
+        line1, ip, steamid = line.split(" | ", maxsplit=2)
+        steamid = steamid[9:]
+        ip = ip[4:]
+        if (steamid not in steams) and (ip not in ips):
+            uniq_ips_and_steamids += 1
+        if steamid not in steams:
+            steams.append(steamid)
+            uniq_steamids += 1
+        if ip not in ips:
+            ips.append(ip)
+            uniq_ips += 1
+    input_file.close()
+    write_to_file(uniq_ips, uniq_steamids, uniq_ips_and_steamids)
